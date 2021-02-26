d1755bdbe3cf9dea1cfac357dd6795a59ca4fe05
Pavel Lapatsin <p.lapatsin@godeltech.com>
2021-02-26 11:20:00 +0000

    Initial

---
+++ src/HiddenVarnulableLibrary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nancy.ModelBinding;
+
+namespace HiddenVarnulableLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var model = new VulnerableLibraries.Modules.FooModule();
+            Console.WriteLine($"{model.Context}");
+        }
+    }
+}
